d72d1bc561c505d8d4eae2dd663cb9285c6540b8
Edson Lima <dddwebdeveloper@gmail.com>
2014-11-30 18:03:13 -0200

    Criada o método para calcular o Gradiente Descendente

---
+++ src/gradientDescent.m
@@ -0,0 +1,11 @@
+function [theta, J_history] = gradientDescent(X, y, theta, alpha, num_iters)
+	m = length(y);
+	J_history = zeros(num_iters, 1);
+
+	for iter = 1:num_iters
+	    temp0 = theta(1) - alpha * (1/m) * sum(X * theta -y);
+	    temp1 = theta(2) - alpha * (1/m) * sum((X * theta -y ).* X(:,2));
+	    theta = [temp0; temp1];
+	    J_history(iter) = computeCost(X, y, theta);
+	end
+end
